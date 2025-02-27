d5587a3996d91ebd1f462ad5c060c71d76a347b8
SrinaThChettiar <149867158+srinathchettiar07@users.noreply.github.com>
2025-02-27 23:28:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let posts = [
     title: "The Rise of Decentralized Finance",
     content:
       "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
-    author: "Alex Thompson",
+    author: "Alex hompson",
     date: "2023-08-01T10:00:00Z",
   },
   {
